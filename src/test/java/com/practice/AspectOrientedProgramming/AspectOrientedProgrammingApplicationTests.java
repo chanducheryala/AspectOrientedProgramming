3fbcdc89d1f50f98f94c9a88146490378e5ea7be
chandu <cheryalachandu33@gmail.com>
2023-11-10 14:33:44 +0530

    AOP

---
+++ src/test/java/com/practice/AspectOrientedProgramming/AspectOrientedProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.AspectOrientedProgramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOrientedProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
